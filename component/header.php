f061b830dc5a4ae790253144e63eb7c27940aa1b
Thiri Win <thiriwinzhuang@gmail.com>
2024-05-07 18:13:16 +0630

    article index

--- component/header.php
+++ component/header.php
@@ -29,7 +29,7 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="index.php">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -38,16 +38,16 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Blog</a>
+                        <a class="nav-link" href="index.php">Blog</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
+                        <a class="nav-link" href="login.php">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Register</a>
+                        <a class="nav-link" href="register.php">Register</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logout</a>
+                        <a class="nav-link" href="logout.php">Logout</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
--- index.php
+++ index.php
@@ -1,6 +1,43 @@
+<?php
+
+    include "db.php";
+    $query = "SELECT * FROM articles";
+    $result = mysqli_query($conn, $query);
+    if($result) {
+        $articles = $result;
+    } else {
+        echo mysqli_connect_error();
+    }
+
+?>
+
+
 <?php include('component/header.php') ?>
 
-    <h1>Index Page</h1>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+        <?php while($article = mysqli_fetch_assoc($articles)): ?>
+        <div class="col">
+            <div class="card">
+                <img src="<?= $article['image'] ?>" alt="Article Picture Here ..." height="250rem" class="object-fit-cover">
+                <div class="card-body">
+                    <h3 class="card-title"><?= htmlspecialchars_decode($article['title']) ?></h3>
+                    <p class="card-text">
+                        <?php 
+                            $str_to_array = explode(" ", htmlspecialchars_decode($article['about'])); 
+                            $slice_array = array_slice($str_to_array, 0, 15);
+                            $result = implode(" ", $slice_array);
+                            $result .= " ... "; 
+                            print_r($result);
+                        ?>
+                    </p>
+                </div>
+                <div class="card-footer text-end">
+                    <a href="article-view.php?id=<?= $article['id'] ?>">See more</a>
+                </div>
+            </div>
+        </div>
+        <?php endwhile; ?>
+    </div>
 
 <?php include('component/footer.php') ?>
 
