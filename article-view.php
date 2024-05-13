1c5e1886c23edb5ec24a0601475e765566ae6f97
Thiri Win <thiriwinzhuang@gmail.com>
2024-05-13 17:33:41 +0630

    article view edit for no id

--- article-view.php
+++ article-view.php
@@ -1,20 +1,29 @@
 <?php
-    include "db.php";
+
+include 'db.php';
+
+if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $query = "SELECT * FROM articles WHERE id='$id'";
     $result = mysqli_query($conn, $query);
-    if($result) {
+    if ($result) {
         $article = mysqli_fetch_assoc($result);
     } else {
         echo mysqli_connect_error();
     }
+}
+
 ?>
 
-<?php include "component/header.php" ?>
+<?php include 'component/header.php'; ?>
+    <?php if(isset($article)): ?>
+        <h2 class="mb-2"><?= $article['title'] ?></h2>
+        <p class="mb-2"><?= $article['about'] ?></p>
+        <img src="<?= $article['image'] ?>" alt="" height="300rem" class="mb-2">
+        <p>post at <i><?= $article['posted_at'] ?></i></p>
+    <?php else: ?>
+        <p class="fs-2 text-center">No Match article.</p>    
+    <?php endif; ?>
 
-<h2 class="mb-2"><?= $article['title'] ?></h2>
-<p class="mb-2"><?= $article['about'] ?></p>
-<img src="<?= $article['image'] ?>" alt="" height="300rem" class="mb-2">
-<p>post at <i><?= $article['posted_at'] ?></i></p>
 
-<?php include "component/footer.php" ?>
+<?php include 'component/footer.php'; ?>
